8db082a25c4314dcb20947036badc901363fa202
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-03 15:37:47 -0300

    añadi soporte para algunos atributos html, cambie el estilo de dos elementos y la sintaxis de las variables en las plantillas

--- tools/templateToHTML.js
+++ tools/templateToHTML.js
@@ -3,15 +3,25 @@
 function parseTagObj(element, data) {
   let content = ''
 
+  let attributes = ''
+
+  for (let att_label in element) {
+    if (att_label !== 'tag' && att_label !== 'content') {
+      let attribute = `${att_label}="${parseTemplate(element[att_label], data)}"`
+      attributes += attribute
+    }
+  }
+
   for (let template of element.content) {
     content += parseTemplate(template, data)
   }
 
-  return `<${element.tag} class="${element.class}">${content}</${element.tag}>`
+  return `<${element.tag} ${attributes}>${content}</${element.tag}>`
 }
 
 function parseTemplate(template, data) {
-  let pattern = /{[^}]+}/
+  console.log(template)
+  let pattern = /\${[^}]+}/
 
   if (typeof template === 'object') {
     return parseTagObj(template, data)
