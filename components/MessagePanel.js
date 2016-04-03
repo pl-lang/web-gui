86eda8a511703719d654badf2e6f8dcc805716c8
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-03 04:35:45 -0300

    modulo para el panel de mensajes

---
+++ components/MessagePanel.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const Emitter = require('emitter')
+const $ = require('jquery')
+
+class StatusBar {
+  constructor(container) {
+    this.container = container
+
+    this.element = this.container.append($(
+      `<div id="status_bar" class="bar bar-top-border flex-row center-align"><span id="status_msg"></span></div>`
+    ))
+
+    this.status_msg = this.element.find('#status_msg')
+
+    this.setTitle('Listo.')
+  }
+
+  setTitle(title) {
+    this.status_msg.text(title)
+  }
+
+  setErrorTitle(error_count) {
+    this.status_msg.empty()
+
+    if (error_count === 0) {
+      this.status_msg.text('Listo.')
+    }
+    else {
+      if (error_count >= 1) {
+        let icon = '<span class="octicon octicon-alert"></span>'
+
+        if (error_count === 1) {
+          this.status_msg.html(`${icon} Se ha encontrado un error en tu programa.`)
+        }
+        else {
+          this.status_msg.html(`${icon} Se han encontrado ${error_count} errores en tu programa.`)
+        }
+      }
+    }
+  }
+
+}
+
+
+class MessagePanel extends Emitter {
+  constructor(container) {
+    this.container = container
+
+    this.setUp()
+  }
+
+  setUp() {
+    this.status_bar = new StatusBar(this.container)
+
+    this.message_list = this.container.append($(
+      '<div id="message_list"></div>'
+    ))
+  }
+}
+
+module.exports = MessagePanel
