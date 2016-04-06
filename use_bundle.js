da8b275c4b93022e6f6d806dfedfe52d6fd7657f
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-06 01:56:28 -0300

    use_bundle actualizado y script para llamar a moduleserve

--- use_bundle.js
+++ use_bundle.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const fs = require('fs')
 
-let file_path = path.resolve('web/app.html').replace('.web', 'web')
+let file_path = path.resolve('web/index.html').replace('.web', 'web')
 
 let string = fs.readFileSync(file_path, 'utf-8')
 
@@ -13,6 +13,6 @@ let replacement = '<script src="./bundle.js" charset="utf-8"></script>'
 
 let new_string = `${string.slice(0, string.search(target) + target.length)}\n  ${replacement}\n</html>`
 
-let index_path = path.resolve('web/index.html')
+let index_path = path.resolve('web/index_with_bundle.html')
 
 fs.writeFileSync(index_path, new_string, 'utf-8')
